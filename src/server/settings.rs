63e7c26114f95e930a11dacf87ca5d0954adc617
abhirag <abhirag.awasthi@verse.in>
2023-11-22 23:47:23 +0530

    Update settings - Expose settings module - Remove config file format constraint

--- src/server/settings.rs
+++ src/server/settings.rs
@@ -1,5 +1,5 @@
 use anyhow::Context;
-use config::{Config, File, FileFormat};
+use config::{Config, File};
 use parking_lot::RwLock;
 
 lazy_static! {
@@ -24,13 +24,14 @@ pub fn load_global_config(base_dir: &str, env: &str) -> anyhow::Result<()> {
     let mut write_guard = settings().write();
 
     let mut builder = Config::builder();
-    let default_config_file = format!("{}/service-default.yml", base_dir);
-    builder = builder.add_source(File::new(&default_config_file, FileFormat::Yaml));
+    let default_config_file = format!("{}/service-default", base_dir);
+    builder = builder.add_source(File::with_name(&default_config_file));
 
-    let env_config_file = format!("{}/service-{}.yml", base_dir, env);
-    builder = builder.add_source(File::new(&env_config_file, FileFormat::Yaml));
+    let env_config_file = format!("{}/service-{}", base_dir, env);
+    builder = builder.add_source(File::with_name(&env_config_file));
 
-    let config = builder.build()
+    let config = builder
+        .build()
         .with_context(|| format!("Error in loading config from dir: {} for env: {}", base_dir, env))?;
 
     *write_guard = config;
